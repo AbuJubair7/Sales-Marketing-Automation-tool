92109d43474c708dc24e455353af9fd5f8f56a3a
mahjabin-mim <mahjabinmim711@gmail.com>
2023-11-14 01:12:23 +0600

    Contact Modify

---
+++ src/entities/contact.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity('contact')
+export class Contact{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    name: string;
+
+    @Column({ unique: true, nullable: false })
+    email: string;
+
+    @Column({ unique: true, nullable: false })
+    mobile: number;
+
+    @Column({ nullable: false })
+    gender: string;
+
+    @Column({ nullable: false })
+    address: string;
+
+    // @BeforeInsert()
+    // async hashPassword() {
+    //     this.password = await bcrypt.hash(this.password, 10);
+    // }
+}
+
